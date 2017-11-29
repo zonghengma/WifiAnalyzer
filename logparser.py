76e2b5391c2ebdb116b5237f09773e60378582f3
Zongheng Ma <zonghengma@gmail.com>
2017-11-29 14:40:20 -0800

    parse logs with LogParser

--- logparser.py
+++ logparser.py
@@ -1,20 +1,11 @@
 # Parse the log file using the search terms in the list
 # Write results to a separate file
-
-search_terms = ["wlan0: Associated with", "wlan0: State", "wlan0: CTRL-EVENT",
-                "wlan0: Considering connect", "wlan0: Request", "selected BSS"]
-#search_terms = ["wpa_supplicant"]
-
-
-filename_final = './logfile_parsed.txt'
-logfile = './log.txt'
-ff = open(filename_final, 'w')
-
-print('parsing logfile...')
-with open(logfile) as f:
-    for line in f:
-        if any(s in line for s in search_terms):
-             ff.write(line)
-
-print('finished')             
-ff.close()
+class LogParser:
+    def __init__(self):
+        self.search_terms = ["wlan0: Associated with", "wlan0: State", "wlan0: CTRL-EVENT","wlan0: Considering connect", "wlan0: Request", "selected BSS"]
+    #search_terms = ["wpa_supplicant"]
+    
+    def parse(self, line):
+        if any(s in line for s in self.search_terms):
+            return(line)
+        return ""
