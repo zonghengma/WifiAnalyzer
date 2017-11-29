598a4e99a8fa5258c9a7c54025b409abf02f17fe
Zongheng Ma <zonghengma@gmail.com>
2017-11-29 14:43:11 -0800

    push readme.txt

--- push.sh
+++ push.sh
@@ -1,2 +1,3 @@
 adb push main.mi2app /sdcard/mobileinsight/plugins/WifiAnalyzer/main.mi2app
 adb push logparser.py /sdcard/mobileinsight/plugins/WifiAnalyzer/logparser.py
+adb push readme.txt /sdcard/mobileinsight/plugins/WifiAnalyzer/readme.txt
